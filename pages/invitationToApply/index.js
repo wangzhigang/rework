fabcef84c0f53b4ded79bf00a13815117321c53f
wangzhigang <4241089@qq.com>
2024-10-15 23:59:53 +0800

    开发新内容

---
+++ pages/invitationToApply/index.js
@@ -0,0 +1,23 @@
+// index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+  onShow(){
+  },
+  handlerGohomeClick() {
+    console.log(12121)
+    wx.navigateTo({
+      url: '../index/index'
+    });
+  },
+})
